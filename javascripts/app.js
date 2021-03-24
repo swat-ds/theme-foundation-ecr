ca03cc29c3476b7747e101ae9d106a53e527bae5
Nabil Kashyap <n@nabilk.com>
2021-03-24 02:12:27 -0400

    adds scrollto plugin and auto scroll on time update

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,39 @@
   $(document).ready(function() {
       $('.search-toggle').click(function() {
         $('#search-container').toggleClass('open').toggleClass('closed');
-      })
+      });
+
+      /* for oral history view adds timeupdate listener */
+      if ( $('audio').length ) {
+        const $audio = $('audio');
+        const idString = '#oral-history-item-type-metadata-transcription';
+        const $transcript = $(idString);
+        let currIndex = 0;
+        let timestamps = $(idString + ' p')
+              .map( (i,v) => [ 
+                [Number(v.dataset.timestart), Number(v.dataset.timestop)] 
+              ] );
+
+        $audio.on('timeupdate', function(d) { 
+          let currTime = d.currentTarget.currentTime * 1000;
+
+          if ( 
+            currTime > timestamps[currIndex][0] 
+            && currTime <= timestamps[currIndex][1] 
+          ) { 
+            
+            let currSection = $(`${idString} p[data-timestart=${timestamps[currIndex][0]}]`);
+
+            currSection.toggleClass('active').delay(1000)
+              .queue(function (next) { 
+                $(this).toggleClass('active'); 
+                next(); 
+              });
+
+              $transcript.scrollTo(currSection, {duration:250, offset: -75})
+            currIndex++;
+          }
+        } );
+      }
   });
 })(jQuery)
